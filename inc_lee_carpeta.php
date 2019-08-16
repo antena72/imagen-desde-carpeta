301d8f5e0e9f7d3eb121fcc3c064f0cf0dd103f9
ximena <ximenamay@hotmail.com>
2019-08-16 15:44:57 -0300

    Add files via upload

---
+++ inc_lee_carpeta.php
@@ -0,0 +1,15 @@
+<?php
+// $carpeta_seleccionada = 'nombre de la carpeta';
+$carpeta_seleccionada = 'pescadosjpg';
+
+function lista_archivos($carpeta){
+    $carpeta = __DIR__ ."/".$carpeta."/";
+    $contenido_carpeta = scandir($carpeta);
+    // print_r($contenido_carpeta);
+    $contenido_carpeta = json_encode($contenido_carpeta, JSON_FORCE_OBJECT);
+    echo $contenido_carpeta;
+}
+
+lista_archivos($carpeta_seleccionada);
+
+?>
